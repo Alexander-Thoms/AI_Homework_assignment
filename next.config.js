c1a5946bf5f2f2ac3aababbe56dbcceee78c7575
Alexander-Thoms <alex.g.thoms@gmail.com>
2026-07-15 14:00:04 -0400

    chore(deps): upgrade Next.js to 16.2.10 and drop unsupported eslint config
    Remove the deprecated eslint key from next.config.js and pin Next.js 16.2.10 with an updated lockfile.

--- next.config.js
+++ next.config.js
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  eslint: {
-    dirs: ['pages', 'components', 'lib', 'src'],
-  },
+  outputFileTracingRoot: __dirname,
 }
 
 module.exports = nextConfig
